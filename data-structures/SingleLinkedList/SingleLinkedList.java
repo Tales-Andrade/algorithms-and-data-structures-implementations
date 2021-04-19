9a451e6afaec1d49f3a85ba14ea85445d9362167
Tales Andrade <andradetales5@gmail.com>
2021-04-19 13:20:56 -0400

    Finished the Single Linked List Structure

--- data-structures/SingleLinkedList/SingleLinkedList.java
+++ data-structures/SingleLinkedList/SingleLinkedList.java
@@ -13,7 +13,7 @@ public class SingleLinkedList<E>
 
     public void addHead(E element)
     {
-        Node<E> newNode = new Node(element);
+        Node<E> newNode = new Node<E>(element);
 
         if (size == 0)
         {
@@ -29,9 +29,11 @@ public class SingleLinkedList<E>
         size += 1;
     }
 
+    
+
     public void addTail(E element)
     {
-        Node<E> newNode = new Node(element);
+        Node<E> newNode = new Node<E>(element);
         
         if (size == 0)
         {
@@ -50,7 +52,7 @@ public class SingleLinkedList<E>
 
     public void add(int index, E element)
     {
-        if ((index >= 0) && (index < this.size))
+        if ((index >= 0) && (index <= this.size))
         {
             if (index == 0)
             {
@@ -62,7 +64,7 @@ public class SingleLinkedList<E>
             }
             else
             {
-                Node<E> newNode = new Node(element);
+                Node<E> newNode = new Node<E>(element);
                 Node<E> tmp = head;
 
                 for (int i = 0; i < index; i++)
@@ -84,11 +86,146 @@ public class SingleLinkedList<E>
 
     }
 
+    public void remove(E element)
+    {
+        int index = -1;
+
+        Node<E> tmp = head;
+
+        for (int i = 0; i < this.size; i++)
+        {
+            if (tmp.getElement().equals(element))
+            {
+                index = i;
+            }
+
+            tmp = tmp.getNext();
+        }
+
+        if (index != -1)
+        {
+            remove(index);
+        }
+    }
+
+    public E remove(int index)
+    {
+
+        if ((index >= 0) && (index < this.size))
+        {
+            Node<E> tmp = head;
+
+            if (head.getNext() == null)
+            {
+                this.clear();
+
+                return tmp.getElement();
+            }
+
+            if (index == 0)
+            {
+                head = head.getNext();
+
+                size -=  1;
+
+                return head.getElement();
+            }
+            else
+            {
+                for (int i = 0; i < index - 1; i++)
+                {
+                    tmp = tmp.getNext();
+                }
+
+                Node<E> toRemove = tmp.getNext();
+
+                tmp.setNext(toRemove.getNext());
+
+                if (toRemove.getNext() == null)
+                {
+                    tail = tmp;
+                }
+
+                toRemove.setNext(null);
+
+                size -=  1;
+
+                return toRemove.getElement();
+            }
+        }
+        else
+        {
+            throw new IndexOutOfBoundsException();
+        }
+
+    }
+
+    public void set(int index, E element)
+    {
+        if ((index >= 0) && (index < this.size))
+        {
+                Node<E> tmp = head;
+
+                for (int i = 0; i < index; i++)
+                {
+                    tmp = tmp.getNext();
+                }
+
+                tmp.setElement(element);
+        }
+        else
+        {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public E get(int index)
+    {
+        if ((index >= 0) && (index < this.size))
+        {
+                Node<E> tmp = head;
+
+                for (int i = 0; i < index; i++)
+                {
+                    tmp = tmp.getNext();
+                }
+
+                return tmp.getElement();
+        }
+        else
+        {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public boolean isEmpty()
+    {
+        return (this.size == 0);
+    }
+
     public void clear()
     {
         this.head = null;
         this.tail = null;
         size = 0;
+    }
+
+    public int getSize()
+    {
+        return this.size;
+    }
+
+    public void print()
+    {
+        Node<E> tmp = head;
+
+        for (int i = 0; i < size; i++)
+        {
+            System.out.print(" " + tmp.getElement().toString());
+            tmp = tmp.getNext();
+        }
+
+        System.out.println();
     }
 
     private class Node<E>
@@ -125,5 +262,21 @@ public class SingleLinkedList<E>
 
     public static void main(String[] args)
     {
+        SingleLinkedList<String> sll = new SingleLinkedList<String>();
+
+        sll.addHead("Hello,");
+        System.out.println(sll.getSize());
+        sll.add(1, "I am a");
+        sll.addTail("wizard!");
+
+        sll.print();
+
+        sll.set(2, "warrior!");
+
+        sll.print();
+
+        sll.remove(1);
+
+        sll.print();
     }
 }
