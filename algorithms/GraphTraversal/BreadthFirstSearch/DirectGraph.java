0744750c2562de9dfaa51c5674aa416fc6508050
Tales Andrade <andradetales5@gmail.com>
2021-05-01 13:05:54 -0400

    Updated the tests for the breadth first search algorithm

--- algorithms/GraphTraversal/BreadthFirstSearch/DirectGraph.java
+++ algorithms/GraphTraversal/BreadthFirstSearch/DirectGraph.java
@@ -15,8 +15,10 @@ public class DirectGraph<T>{
         return vertexMap.get(vertex.getElement());
     }
 
-    public void addEdge(Vertex<T> vertex, Edge<T> edge){
-        vertex.addEdge(edge);
+    public void addEdge(Vertex<T> vertexOne, Vertex<T> vertexTwo){
+        Edge<T> edge = new Edge<T>(vertexTwo);
+
+        vertexOne.addEdge(edge);
     }
 
     public void removeEdge(Vertex<T> vertex, Edge<T> edge){
